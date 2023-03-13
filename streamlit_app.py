18b5805ac65d180f7f291905806b60ec737e9cb3
gabrielgmendonca <gabrielgmendonca@poli.ufrj.br>
2023-03-13 10:23:01 -0300

    feat: add simple authentication

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,28 @@ import streamlit as st
 from llm import LLM
 
 
-llm = LLM('index_a_caminho_da_luz.json')
+def check_password():
+    def password_entered():
+        if st.session_state['password'] == st.secrets['password']:
+            st.session_state['password_correct'] = True
+            del st.session_state['password']  # don't store password
+        else:
+            st.session_state['password_correct'] = False
+
+    if 'password_correct' not in st.session_state:
+        st.text_input(
+            'Password', type='password', on_change=password_entered, key='password'
+        )
+        return False
+    elif not st.session_state['password_correct']:
+        st.text_input(
+            'Password', type='password', on_change=password_entered, key='password'
+        )
+        st.error('😕 Password incorrect')
+        return False
+    else:
+        return True
+
 
 st.title('A Caminho da Luz')
 
@@ -11,16 +32,19 @@ Nessa demo, o ChatGPT tentará responder às suas perguntas usando o livro
 "A Caminho da Luz" de Emmanuel / Chico Xavier como base.
 """
 
-st.markdown("""
-Exemplos de perguntas:
-- Qual o papel de Jesus na criação da Terra?
-- O que você sabe sobre a criação da lua?
-- O que você pode me contar sobre a família romana?
-- Quem foram os mahatmas?
-- Quem foi Fo-Hi?
-""")
+if check_password():
+    st.markdown("""
+    Exemplos de perguntas:
+    - Qual o papel de Jesus na criação da Terra?
+    - O que você sabe sobre a criação da lua?
+    - O que você pode me contar sobre a família romana?
+    - Quem foram os mahatmas?
+    - Quem foi Fo-Hi?
+    """)
 
-question = st.text_input('O que você gostaria de saber com base no livro "A Caminho da Luz"?')
-if question:
-    answer = llm.query(question)
-    st.write(answer)
+    llm = LLM('index_a_caminho_da_luz.json')
+
+    question = st.text_input('O que você gostaria de saber com base no livro "A Caminho da Luz"?')
+    if question:
+        answer = llm.query(question)
+        st.write(answer)
